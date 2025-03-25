16c1b96d611c4f6f70475ee157d09ef6fa6d2807
Vivek Patil <vivekjpatil1357@gmail.com>
2025-03-25 16:23:13 +0530

    server.ts changes

--- backend/server.ts
+++ backend/server.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/', LawyerRouter)
+// app.use('/', LawyerRouter)
 app.use(RegisterRouter)
 app.use('/api/lawyers', LawyerRouter)
 app.use('/api/chats', ChatRouter)
